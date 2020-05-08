940b55d715218b9ff77ace3b4ee23a97e71a90c2
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-08 23:25:36 +0100

    Method to partition file into recipes

--- partition.cpp
+++ partition.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "partition.hpp"
 
 
@@ -5,5 +6,18 @@ using namespace std;
 
 vector<string> recipes(istream &stream) {
   vector<string> result;
+  ostringstream recipe;
+  string line;
+  bool on = false;
+  while (getline(stream, line)) {
+    if (line.rfind("MMMMM", 0) == 0 && line.length() > 5)
+      on = true;
+    if (on)
+      recipe << line << endl;
+    if (line == "MMMMM")
+      on = false;
+  };
+  if (!recipe.str().empty())
+    result.push_back(recipe.str());
   return result;
 }
--- test_partition.cpp
+++ test_partition.cpp
@@ -8,8 +8,27 @@ using namespace testing;
 using namespace std;
 
 
-TEST(PartitionTest, EmptyFile)
-{
+TEST(PartitionTest, EmptyFile) {
   istringstream s("");
   ASSERT_TRUE(recipes(s).empty());
+}
+
+TEST(PartitionTest, MinimalRecipe) {
+  istringstream s("MMMMM---MEAL-MASTER Format---\nMMMMM");
+  EXPECT_EQ(1, recipes(s).size());
+}
+
+TEST(PartitionTest, RecipeContent) {
+  istringstream s("MMMMM---MEAL-MASTER Format---\nMMMMM");
+  EXPECT_EQ("MMMMM---MEAL-MASTER Format---\nMMMMM\n", recipes(s)[0]);
+}
+
+TEST(PartitionTest, IgnoreStrayText) {
+  istringstream s("text");
+  ASSERT_TRUE(recipes(s).empty());
+}
+
+TEST(PartitionTest, EndOfRecipe) {
+  istringstream s("MMMMM---MEAL-MASTER Format---\nMMMMM\ntext");
+  EXPECT_EQ("MMMMM---MEAL-MASTER Format---\nMMMMM\n", recipes(s)[0]);
 }
