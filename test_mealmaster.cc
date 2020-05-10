6d017a06ea2d523d38eb4bd741add11209101a6c
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-10 22:42:45 +0100

    Renamed test fixtures

--- test_mealmaster.cc
+++ test_mealmaster.cc
@@ -9,13 +9,13 @@ using namespace std;
 
 
 TEST(MealMasterTest, RecipeTitle) {
-  ifstream f("test.mmf");
+  ifstream f("test_header.mmf");
   auto result = parse_mealmaster(f);
   EXPECT_EQ("apple pie", result.title());
 }
 
 TEST(MealMasterTest, Categories) {
-  ifstream f("test.mmf");
+  ifstream f("test_header.mmf");
   auto result = parse_mealmaster(f);
   ASSERT_EQ(2, result.categories().size());
   EXPECT_EQ("pastries", result.categories()[0]);
@@ -23,7 +23,7 @@ TEST(MealMasterTest, Categories) {
 }
 
 TEST(MealMasterTest, Servings) {
-  ifstream f("test2.mmf");
+  ifstream f("test_header2.mmf");
   auto result = parse_mealmaster(f);
   EXPECT_EQ(42, result.servings());
   EXPECT_EQ("servings", result.servings_unit());
