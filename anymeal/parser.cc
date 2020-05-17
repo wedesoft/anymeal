f65bfc4bda6ace4a1f80871050624a3c9a3b6bf9
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-17 20:22:35 +0100

    Added some more output

--- anymeal/parser.cc
+++ anymeal/parser.cc
@@ -12,15 +12,18 @@ int main(int argc, char *argv[]) {
     cerr << "Syntax: " << argv[0] << " [filename] ..." << endl;
     return 1;
   };
+  cout << "Processing " << argc << " files\r\n";
   for (int i=1; i<argc; i++) {
     ifstream f(argv[i]);
     auto lst = recipes(f);
+    cout << "File " << argv[i] << " has " << lst.size() << " recipes\r\n";
     for (auto recipe=lst.begin(); recipe!=lst.end(); recipe++) {
       istringstream s(*recipe);
       try {
         auto result = parse_mealmaster(s);
         cout << "Parsed recipe: " << result.title() << "\r\n";
       } catch (parse_exception &e) {
+        cout << "Rejected recipe" << "\r\n";
         cout << e.what() << endl;
         cout << *recipe;
       };
