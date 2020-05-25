7007c7f6fd3a3abd24f32f6493a20125377b4c19
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-25 21:30:07 +0100

    Connect to database when starting GUI

--- anymeal/main_window.cc
+++ anymeal/main_window.cc
@@ -1,6 +1,30 @@
+#include <unistd.h>
+#include <QtCore/QStandardPaths>
+#include <QtWidgets/QFileDialog>
+#include <QtWidgets/QMessageBox>
 #include "main_window.hh"
 
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
-  ui.setupUi(this);
+  m_ui.setupUi(this);
+  connect(m_ui.action_import, &QAction::triggered, this, &MainWindow::import);
+  try {
+    auto path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QDir dir(path);
+    dir.mkpath(dir.absolutePath());
+    m_database.open(dir.filePath("anymeal.db").toUtf8().constData());
+  } catch (database_exception &e) {
+    QMessageBox::critical(this, "Error opening database", e.what());
+    exit(1);
+  };
+}
+
+void MainWindow::import(void) {
+  QStringList result =
+    QFileDialog::getOpenFileNames(this, "Import MealMaster files", "", "MealMaster (*.mm *.MM *.mmf *.MMF);;Text (*.txt *.TXT);;"
+                                  "All files (*)");
+  if (!result.isEmpty()) {
+    for (int i=0; i<result.size(); i++) {
+    };
+  };
 }
--- anymeal/main_window.hh
+++ anymeal/main_window.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include <QtWidgets/QMainWindow>
 #include "ui_main_window.hh"
+#include "database.hh"
 
 
 class MainWindow: public QMainWindow
@@ -8,6 +9,9 @@ class MainWindow: public QMainWindow
   Q_OBJECT
 public:
   MainWindow(QWidget *parent=nullptr);
+public slots:
+  void import(void);
 protected:
-  Ui::MainWindow ui;
+  Ui::MainWindow m_ui;
+  Database m_database;
 };
