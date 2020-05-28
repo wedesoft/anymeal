b73d57e744388a1e1a395fc17c59872544814164
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-05-28 21:59:34 +0100

    Working on HTML conversion

--- anymeal/html.cc
+++ anymeal/html.cc
@@ -4,7 +4,54 @@
 
 using namespace std;
 
+string html_encode(const string &data) {
+  // https://stackoverflow.com/questions/5665231/most-efficient-way-to-escape-xml-html-in-c-string
+  string result;
+  result.reserve(data.size());
+  for(size_t pos=0; pos!=data.size(); pos++) {
+    switch(data[pos]) {
+      case '&':  result.append("&amp;");       break;
+      case '\"': result.append("&quot;");      break;
+      case '\'': result.append("&apos;");      break;
+      case '<':  result.append("&lt;");        break;
+      case '>':  result.append("&gt;");        break;
+      default:   result.append(&data[pos], 1); break;
+    };
+  };
+  return result;
+}
+
 string recipe_to_html(Recipe &recipe) {
   ostringstream stream;
+  stream << "<html>\n";
+  stream << "  <head>\n";
+  if (!recipe.title().empty()) {
+    stream << "    <title>" << html_encode(recipe.title()) << "</title>\n";
+  };
+  stream << "  </head>\n";
+  stream << "  <body>\n";
+  if (!recipe.title().empty()) {
+    stream << "    <h2>" << html_encode(recipe.title()) << "</h2>\n";
+  };
+  if (!recipe.categories().empty()) {
+    stream << "    <p><b>Categories:</b> ";
+    for (auto category=recipe.categories().begin(); category!=recipe.categories().end(); category++) {
+      stream << html_encode(*category);
+      if (category + 1 != recipe.categories().end())
+        stream << ", ";
+    };
+    stream << "</p>\n";
+  };
+  if (recipe.servings() > 0) {
+    stream << "    <p><b>Yield:</b> " << recipe.servings() << " " << html_encode(recipe.servings_unit()) << "</p>\n";
+  };
+  if (!recipe.instructions().empty()) {
+    stream << "    <h3>Instructions</h3>\n";
+  };
+  for (auto instruction=recipe.instructions().begin(); instruction!=recipe.instructions().end(); instruction++) {
+    stream << "    <p>" << html_encode(*instruction) << "</p>\n";
+  };
+  stream << "  </body>\n";
+  stream << "</html>";
   return stream.str();
 }
--- tests/test_html.cc
+++ tests/test_html.cc
@@ -6,8 +6,47 @@
 using namespace testing;
 using namespace std;
 
+TEST(HTMLTest, EmptyRecipe) {
+  Recipe recipe;
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n  </head>\n  <body>\n  </body>\n</html>", result);
+}
+
 TEST(HTMLTest, RecipeTitle) {
   Recipe recipe;
   recipe.set_title("Title");
-  auto result = recipe_to_html(recipe);
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n    <title>Title</title>\n  </head>\n  <body>\n    <h2>Title</h2>\n  </body>\n</html>", result);
+}
+
+TEST(HTMLTest, EscapeTitle) {
+  Recipe recipe;
+  recipe.set_title("B & B");
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n    <title>B &amp; B</title>\n  </head>\n"
+            "  <body>\n    <h2>B &amp; B</h2>\n  </body>\n</html>", result);
+}
+
+TEST(HTMLTest, Instructions) {
+  Recipe recipe;
+  recipe.add_instruction("Mix & shake.");
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n  </head>\n  <body>\n    <h3>Instructions</h3>"
+            "\n    <p>Mix &amp; shake.</p>\n  </body>\n</html>", result);
+}
+
+TEST(HTMLTest, Servings) {
+  Recipe recipe;
+  recipe.set_servings(4);
+  recipe.set_servings_unit("servings");
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n  </head>\n  <body>\n    <p><b>Yield:</b> 4 servings</p>\n  </body>\n</html>", result);
+}
+
+TEST(HTMLTest, Categories) {
+  Recipe recipe;
+  recipe.add_category("bread");
+  recipe.add_category("brewing");
+  string result = recipe_to_html(recipe);
+  EXPECT_EQ("<html>\n  <head>\n  </head>\n  <body>\n    <p><b>Categories:</b> bread, brewing</p>\n  </body>\n</html>", result);
 }
