dc2c4187759cf0f55d6302451084253de62a4796
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-06-06 21:09:31 +0100

    Add German translation

---
+++ anymeal/anymeal_de.ts
@@ -0,0 +1,255 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ImportDialog</name>
+    <message>
+        <location filename="import_dialog.ui" line="14"/>
+        <source>Import MealMaster Recipes</source>
+        <translation>Importiere MealMaster Rezepte</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="26"/>
+        <source>En&amp;coding</source>
+        <translation>&amp;Zeichensatz</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="40"/>
+        <source>ISO-8859-1</source>
+        <translation>ISO-8859-1</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="45"/>
+        <source>ISO-8859-15</source>
+        <translation>ISO-8859-15</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="50"/>
+        <source>CP850</source>
+        <translation>CP850</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="55"/>
+        <source>US-ASCII</source>
+        <translation>US-ASCII</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="63"/>
+        <source>Error Fi&amp;le</source>
+        <translation>Feh&amp;lerdatei</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="75"/>
+        <source>/tmp/errors.mmf</source>
+        <translation>/tmp/fehler.mmf</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="85"/>
+        <source>&amp;Select error file</source>
+        <translation>Fehlerdatei aus&amp;wählen</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="124"/>
+        <source>Pr&amp;oceed with selection of recipes</source>
+        <translation>Mit Auswahl der Rezepte f&amp;ortfahren</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.ui" line="131"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Abbruch</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.cc" line="15"/>
+        <source>errors.mmf</source>
+        <translation>fehler.mmf</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.cc" line="27"/>
+        <source>Select Error File</source>
+        <translation>Fehlerdatei auswählen</translation>
+    </message>
+    <message>
+        <location filename="import_dialog.cc" line="27"/>
+        <source>MealMaster (*.mm *.MM *.mmf *.MMF);;Text (*.txt *.TXT);;All files (*)</source>
+        <translation>MealMaster (*.mm *.MM *.mmf *.MMF);;Text (*.txt *.TXT);;Alle Dateien (*)</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_window.ui" line="14"/>
+        <source>AnyMeal</source>
+        <translation>AnyMeal</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="27"/>
+        <source>&amp;Show all recipes</source>
+        <translation>Alle Rezepte an&amp;zeigen</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="34"/>
+        <source>&amp;Title</source>
+        <translation>&amp;Titel</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="47"/>
+        <source>&amp;Category</source>
+        <translation>&amp;Kategorie</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="60"/>
+        <source>&amp;Ingredient</source>
+        <translation>Z&amp;utat</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="73"/>
+        <source>&amp;Filter</source>
+        <translation>&amp;Filtern</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="127"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="137"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="147"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="150"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="155"/>
+        <source>&amp;Import</source>
+        <translation>&amp;Importieren</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="158"/>
+        <source>Ctrl+I</source>
+        <translation>Ctrl+I</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="163"/>
+        <source>Print p&amp;review</source>
+        <translation>&amp;Druckvorschau</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="166"/>
+        <source>Ctrl+R</source>
+        <translation>Ctrl+R</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="171"/>
+        <source>&amp;Print recipe</source>
+        <translation>Rezept d&amp;rucken</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="174"/>
+        <source>Ctrl+P</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="179"/>
+        <source>A&amp;bout AnyMeal</source>
+        <translation>Ü&amp;ber AnyMeal</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="182"/>
+        <source>Ctrl+B</source>
+        <translation>Ctrl+B</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="187"/>
+        <source>&amp;Delete recipes</source>
+        <translation>Rezepte &amp;löschen</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="190"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="49"/>
+        <source>Error Opening Database</source>
+        <translation>Fehler beim Öffnen der Datenbank</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="62"/>
+        <source>Import MealMaster Files</source>
+        <translation>Importiere MealMaster Dateien</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="62"/>
+        <source>MealMaster (*.mm *.MM *.mmf *.MMF);;Text (*.txt *.TXT);;All files (*)</source>
+        <translation>MealMaster (*.mm *.MM *.mmf *.MMF);;Text (*.txt *.TXT);;Alle Dateien (*)</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="66"/>
+        <source>Importing files ...</source>
+        <translation>Importiere Dateien ...</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="66"/>
+        <source>Cancel</source>
+        <translation>Abbruch</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="71"/>
+        <source>Processing file %1 ...</source>
+        <translation>Verarbeite Datei %1 ...</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="83"/>
+        <source>Rejected recipe: </source>
+        <translation>Rezept abgelehnt: </translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="88"/>
+        <source>Error writing to file </source>
+        <translation>Fehler beim Schreiben der Datei </translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="112"/>
+        <source>Error While Importing</source>
+        <translation>Fehler beim Importieren</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="160"/>
+        <source>Error Filtering Recipes</source>
+        <translation>Fehler beim Filtern der Rezepte</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="173"/>
+        <source>Error Resetting Selection</source>
+        <translation>Fehler beim Zurücksetzen der Auswahl</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="185"/>
+        <source>Error fetching recipe</source>
+        <translation>Fehler beim Abrufen des Rezepts</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="197"/>
+        <source>Delete Recipes</source>
+        <translation>Rezept löschen</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="197"/>
+        <source>Do you want to delete the selected recipes?</source>
+        <translation>Wollen Sie die ausgewählten Rezepte löschen?</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="210"/>
+        <source>Error Deleting Recipes</source>
+        <translation>Fehler beim Löschen der Rezepte</translation>
+    </message>
+</context>
+</TS>
