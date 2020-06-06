4bf4726392064ba28d0f587e917cf208cf609179
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-06-06 23:00:50 +0100

    Add translation

--- anymeal/anymeal.cc
+++ anymeal/anymeal.cc
@@ -1,4 +1,5 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QTranslator>
 #include <QtWidgets/QApplication>
 #include "main_window.hh"
 
@@ -7,6 +8,15 @@ int main(int argc, char *argv[]) {
   QCoreApplication::addLibraryPath(".");
   QApplication app(argc, argv);
   setlocale(LC_NUMERIC, "C"); // Change back behaviour of atof.
+  QString country = QLocale::system().name().mid(0, 2);
+  QTranslator translator;
+#ifdef __MINGW32__
+  QString locale_dir = QString("locale\\%1").arg(country);
+#else
+  QString locale_dir = QString("%1/%2/LC_MESSAGES").arg(LOCALEDIR).arg(country);
+#endif
+  translator.load("anymeal_qt", locale_dir);
+  app.installTranslator(&translator);
   MainWindow main_window;
   main_window.show();
   return app.exec();
