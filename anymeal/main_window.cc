0647df8188340598676b08b0eca43e22520c15df
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-07-21 17:03:33 +0100

    Update copyright date

--- anymeal/main_window.cc
+++ anymeal/main_window.cc
@@ -340,7 +340,7 @@ void MainWindow::open_converter(void) {
 
 void MainWindow::about(void) {
   QMessageBox::information(this, tr("About AnyMeal %1").arg(PACKAGE_VERSION), "AnyMeal recipe management software\n"
-      "Copyright © 2002 to 2023  Jan Wedekind\n\n"
+      "Copyright © 2002 to 2024  Jan Wedekind\n\n"
       "This program is free software: you can redistribute it and/or modify "
       "it under the terms of the GNU General Public License as published by "
       "the Free Software Foundation, either version 3 of the License, or "
